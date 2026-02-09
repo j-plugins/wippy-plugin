77a14770c1c6770ce0f2bda6b1d05c8f6850e911
Dmitriy Derepko <xepozz@list.ru>
2026-02-09 13:03:21 +0400

    fix: bugs with collision

--- src/main/kotlin/com/github/xepozz/wippy/WippyFileType.kt
+++ src/main/kotlin/com/github/xepozz/wippy/WippyFileType.kt
@@ -5,13 +5,10 @@ import org.jetbrains.yaml.YAMLLanguage
 
 class WippyFileType private constructor() : LanguageFileType(YAMLLanguage.INSTANCE), java.io.Serializable {
     override fun getName() = "Wippy File"
-
     override fun getDescription() = "Wippy configuration file"
-
     override fun getDefaultExtension() = "yaml"
-
+    override fun getDisplayName() = "Wippy Config File"
     override fun getIcon() = WippyIcons.WIPPY
-
     companion object {
         @JvmStatic
         val INSTANCE = WippyFileType()
--- src/main/kotlin/com/github/xepozz/wippy/WippyLockFileType.kt
+++ src/main/kotlin/com/github/xepozz/wippy/WippyLockFileType.kt
@@ -6,12 +6,10 @@ import java.io.Serializable
 
 class WippyLockFileType private constructor() : LanguageFileType(YAMLLanguage.INSTANCE), Serializable {
     override fun getName() = "Wippy Lock File"
-
     override fun getDescription() = "Wippy Lock file"
-
     override fun getDefaultExtension() = "lock"
-
     override fun getIcon() = WippyIcons.WIPPY
+    override fun getDisplayName() = "Wippy Lock File"
 
     companion object {
         @JvmStatic
