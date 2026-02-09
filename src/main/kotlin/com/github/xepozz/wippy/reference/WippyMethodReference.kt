c95964e05a1d7723f2f691fc59ec46c249b72487
Dmitriy Derepko <xepozz@list.ru>
2026-02-09 14:16:22 +0400

    feat: broken features

--- src/main/kotlin/com/github/xepozz/wippy/reference/WippyMethodReference.kt
+++ src/main/kotlin/com/github/xepozz/wippy/reference/WippyMethodReference.kt
@@ -1,10 +1,16 @@
 package com.github.xepozz.wippy.reference
 
+import com.github.xepozz.wippy.lang.psi.WippyLuaLocalFuncDefStat
+import com.github.xepozz.wippy.lang.psi.WippyLuaTypes
+import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.icons.AllIcons
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiElementResolveResult
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiPolyVariantReferenceBase
 import com.intellij.psi.ResolveResult
+import com.intellij.psi.util.PsiTreeUtil
 
 class WippyMethodReference(
     element: PsiElement,
@@ -13,22 +19,25 @@ class WippyMethodReference(
     range: TextRange
 ) : PsiPolyVariantReferenceBase<PsiElement>(element, range, /* soft = */ false) {
     override fun multiResolve(p0: Boolean): Array<out ResolveResult?> {
-        return emptyArray()
-//        return PsiTreeUtil
-//            .findChildrenOfAnyType(luaFile, LuaFuncDef::class.java, LuaLocalFuncDef::class.java)
-//            .filter { it.node.findChildByType(LuaTypes.ID)?.text == methodName }
-//            .let { PsiElementResolveResult.createResults(it) }
+//        return emptyArray()
+        return findFunctionByName(luaFile, methodName)
+            .let { PsiElementResolveResult.createResults(it) }
     }
 
     override fun getVariants(): Array<Any> {
-        return emptyArray()
-//        return PsiTreeUtil
-//            .findChildrenOfAnyType(luaFile, LuaFuncDef::class.java, LuaLocalFuncDef::class.java)
-//            .mapNotNull { it.node.findChildByType(LuaTypes.ID)?.text }
-//            .map {
-//                LookupElementBuilder.create(it)
-//                    .withIcon(AllIcons.Nodes.Function)
-//            }
-//            .toTypedArray()
+        return getLocalFunctions(luaFile)
+            .mapNotNull { it.node.findChildByType(WippyLuaTypes.IDENT)?.text }
+            .map {
+                LookupElementBuilder.create(it)
+                    .withIcon(AllIcons.Nodes.Function)
+            }
+            .toTypedArray()
     }
+
+    private fun getLocalFunctions(luaFile: PsiFile): Collection<WippyLuaLocalFuncDefStat> = PsiTreeUtil
+        .findChildrenOfAnyType(luaFile, WippyLuaLocalFuncDefStat::class.java)
+
+    private fun findFunctionByName(luaFile: PsiFile, methodName: String): List<WippyLuaLocalFuncDefStat?> =
+        getLocalFunctions(luaFile)
+            .filter { it.node.findChildByType(WippyLuaTypes.IDENT)?.text == methodName }
 }
